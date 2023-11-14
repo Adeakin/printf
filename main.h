4e928bb9c3ea9bc9f90a3a494ccbf95502ee6ba1
lo-adienge <107340507+lo-adienge@users.noreply.github.com>
2023-11-15 00:47:59 +0300

    Update main.h

--- main.h
+++ main.h
@@ -84,18 +84,7 @@ int hand_width(const char *format, int *c, va_list argsList);
 int hand_precision(const char *format, int *c, va_list argsList);
 int hand_size(const char *format, int *c);
 int handle_print(const char *format, int *c, va_list argsList, char buffer[], int flags, int width, int precision, int size);
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
-int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
+
 
 
 
