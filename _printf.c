5f1e42a1259114ab34f246efca505a3fb7b4170b
Adeakin <abimbolaakinshilo@gmail.com>
2023-11-14 18:39:00 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -4,67 +4,45 @@
 #include <string.h>
 
 /**
- * funcs -  function prototypes
- */
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf- Printf lookalike function
+ * _printf - Printf look alike function
  * @format: input format
  *
- * Return: the number of characters formatted and printed
+ * Return: characters printed 
  */
-
 int _printf(const char *format, ...)
 {
-	/**
-	 * declaration of variable
-	 */
-	int c, printed, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list argsList;
-	char buffer[BUFF_SIZE];
-	
-	/* NULL format check */
+	int spec_char = 0;
+	va_list argslist;
+
 	if (format == NULL)
 		return (-1);
-	
-	va_start(argsList, format);
-	
-	for (c = 0; format && format[c] != '\0'; c++)
+
+	va_start(argslist, format);
+	while (*format)
 	{
-		if (format[c] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[c];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			printed_chars++;
+			switch (*(++format))
+			{
+				case 'c':
+					putchar(va_arg(argslist, int));
+					spec_char++;
+					break;
+				case 's':
+					puts(va_arg(argslist, char *));
+					spec_char++;
+					break;
+				default:
+					putchar(*format);
+					spec_char++;
+			}
 		}
 		else
 		{
-			/* Processing the format specifier */
-			print_buffer(buffer, &buff_ind);
-			flags = hand_flags(format, &c);
-			width = hand_width(format, &c, argsList);
-			precision = hand_precision(format, &c, argsList);
-			size = hand_size(format, &c);
-			++c;
-			printed = handle_print(format, &c, argsList, buffer, flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			putchar(*format);
 		}
+		format++;
 	}
-	print_buffer(buffer, &buff_ind);
-	va_end(argsList);
-	return (printed_chars);
-}
-
-/* Prints the contents of the buffer only if it exists */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	
-	*buff_ind = 0;
+	va_end(argslist);
+	return (spec_char);
 }
