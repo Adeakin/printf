b2cd4b54dc5f8c155fba8e1cb5a5da95f26921e7
lo-adienge <107340507+lo-adienge@users.noreply.github.com>
2023-11-18 04:19:37 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,78 +1,23 @@
-#ifndef FUNCS_H
-#define FUNCS_H
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+
 #include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
-/* THE FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* THE SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-typedef struct fmt fmt_t;
-
+int _putchar(char character);
+int printf_char(va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
+int selector(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *c,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int _x(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
 
-/****************** FUNCTIONS ******************/
-
-/* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-/* Functions to print numbers */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-/* Function to print non printable characters */
-int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-/* Function to print memory address */
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-/* Functions to handle other specifiers */
-int hand_flags(const char *format, int *c);
-int hand_width(const char *format, int *c, va_list argsList);
-int hand_precision(const char *format, int *c, va_list argsList);
-int hand_size(const char *format, int *c);
-int handle_print(const char *fmt, int *ind, va_list argsList, char buffer[], int flags, int width, int precision, int size);
-int handle_print(const char *fmt, int *c, va_list argsList, char buffer[], int flags, int width, int precision, int size);
-
-
-
-#endif /* FUNCS_H */
+#endif
